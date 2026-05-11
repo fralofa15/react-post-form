c8a3fb1038cfc0de09002e337b37c6bffce6e038
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-05-11 17:57:21 +0200

    Aggiunte componenti al form

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,6 +2,25 @@ function Form(){
     return(
         <form>
             <h3>Compila il form</h3>
+            <div>
+                <label>Nome autore</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label>Titolo dell'opera</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label>Commento</label>
+                <textarea rows={4} cols={20}></textarea>
+            </div>
+            <div>
+                <label>Publica</label>
+                <input type="checkbox" />
+            </div>
+            <div>
+                <button>Invia</button>
+            </div>
         </form>
     );
 }
