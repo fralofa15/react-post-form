2c9c89ead54696cdbffc08d38975141ee0c7e843
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-05-11 17:51:56 +0200

    Creata componente form.jsx e aggiunta ad app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Form from "./components/form"
 function App() {
-
+    return(
+        <div>
+            <h1>Crea un post</h1>
+            <Form/>
+        </div>
+    );
 }
 
 export default App
---
+++ src/components/form.jsx
@@ -0,0 +1,8 @@
+function Form(){
+    return(
+        <form>
+            <h3>Compila il form</h3>
+        </form>
+    );
+}
+export default Form
